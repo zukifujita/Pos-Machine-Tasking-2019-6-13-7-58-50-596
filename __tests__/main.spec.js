445f1f16d860cf398da1ea682a95b27540602125
Mizuki Fujita <mizuki.fujita@oocl.com>
2019-10-17 02:47:18 +0900

    Added main.spec.js

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,5 +1,12 @@
 const print = require('../main');
 
-it ('should add two numbers', () => {
-    expect(print("0001")).toBe(5);
+it ('should print receipt', () => {
+    let expectedResult = 'Receipts\n' +
+    '------------------------------------------------------------\n' +
+    'Coca Cola                       3          1\n' +
+    'Pepsi-Cola                      5          2\n' +
+    'Dr Pepper                       7          1\n' + 
+    '------------------------------------------------------------\n' +
+    'Price: 20'
+    expect(print(['0001', '0003', '0005', '0003'])).toBe(expectedResult);
 });
